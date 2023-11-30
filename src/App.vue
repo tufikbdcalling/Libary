6ae2f21b136403e7b973d64cb1d1295cfaa6ec76
Tufik Hasan <tufikhasan05@gmail.com>
2023-11-30 10:34:17 +0600

    multiple ternary operator

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,14 @@ import data from "./data/data";
 </script>
 
 <template>
-  <h1 v-if="data.name == 'Towfik'">{{ data.name }}</h1>
+  <h1
+    :class="[
+      data.name === 'Towfik' ? 'btn-primary' : 'btn-secondary',
+      data.age === 25 ? 'adult' : 'child',
+    ]"
+  >
+    Hello
+  </h1>
 </template>
 
 <style scoped></style>
